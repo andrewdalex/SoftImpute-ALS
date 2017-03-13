d9d3fcf849a69b88ffcdf6243c47462e43332404
Andrew Alex <andrewalex123@ucla.edu>
2017-03-12 17:12:08 -0700

    RMSE down to ~1.06

--- driver-softImpute.py
+++ driver-softImpute.py
@@ -3,15 +3,15 @@ import numpy as np
 import scipy.sparse as sp
 import softImpute_alg
 
-k = 40  # lower rank
-Lambda = 10  # regularization parameter (the same for U and V)
+k = 35  # lower rank
+Lambda = 20  # regularization parameter (the same for U and V)
 num_iter = 20  # number of iterations in ALS
 
 # Read movie names and print your own ratings
 num_users = 943
 num_items = 1682
 item_names = []
-with open('ml-100k/u.item') as f:
+with open('data/ml-100k/u.item') as f:
 	for line in f:
 		item_names.append(line.split('|')[1])
 
@@ -23,16 +23,16 @@ def read_file(filename):
 	ratings = raw_data[:, 2].astype(np.float64)
 	R = sp.coo_matrix((ratings, (users, items)), shape=(num_users, num_items))
 	return R.tocsr()
-R_train = read_file('ml-100k/u2.base')
-R_test = read_file('ml-100k/u2.test')
+R_train = read_file('data/ml-100k/u2.base')
+R_test = read_file('data/ml-100k/u2.test')
 
 U = np.zeros((num_users,k), order='F')
 V = np.zeros((num_items,k), order='F')
 Dsq = np.eye(k)
-U[:] = np.random.normal(num_users*k)
+U[:, :] = np.random.randn(num_users, k)
 
 # Call functions in cf for running ALS
 print('==== Start running ALS ==== ({0:d} iterations, Lambda={1:g})'.format(num_iter, Lambda))
-softImpute_alg.Ssimpute_ALS(R_train, U, V, Dsq, k, 1e-05, Lambda, 20)
-print('RMSE on test data:', softImpute_alg.compute_rmse_UV(R_test, U, V))
+softImpute_alg.Ssimpute_ALS(R_train, U, V, Dsq, k, 1e-05, Lambda, 100)
+print('RMSE on test data:', softImpute_alg.compute_rmse_UV(R_test, U, V, Dsq))
 print('')
--- softImpute_alg.py
+++ softImpute_alg.py
@@ -7,73 +7,94 @@ Created on Wed Mar  8 12:55:30 2017
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as splinalg
+import scipy.linalg as spla
 
 #Requires driver script
 #Broken -- has very high RMSE values
 #Compute softImpute ALS based on https://github.com/cran/softImpute/blob/master/R/Ssimpute.als.R
-# R: Incomplete rating matrix (n-by-m), a sparse matrix in CSR format ('0' means missing entries)
-# U: Randomly chosen n-by-k dense matrix with orthonormal columns
+# R: Incomplete rating matrix (mxn), a sparse matrix in CSR format ('0' means missing entries)
+# U: Randomly chosen m-by-k dense matrix with orthonormal columns
     #U = np.zeros((num_users,k), order='F'))
     #U[:] = np.random.normal(num_users*k)
-# V: Zero m-by-k dense matrix
-   #V = np.zeros((num_items,k), order='F') 
+# V: Zero n-by-k dense matrix
+   #V = np.zeros((num_items,k), order='F')
 # D: np.eye(k)
 # k: lower rank to approximate
 def Ssimpute_ALS(R, U, V, Dsq, k, thresh = 1e-05, Lambda = 0, maxit = 100):
-    n,m = R.shape
-    Rt = R.transpose()    
+    m, n = R.shape
+    Rt = R.transpose()
     row, col = R.nonzero()
-    Xt_hat = np.zeros((m,n))
-    X_hat = np.zeros((n,m))
+    Xt_star = np.zeros((n,m))
+    X_star = np.zeros((m,n))
     #nonzero = np.zeros((n,m))
     #nonzero[row,col] = 1
     A = np.dot(U, Dsq)
     B = np.dot(V, Dsq)
+    U_old = np.zeros((m, k))
+    V_old = np.zeros((n, k))
+    Dsq_old = np.eye(k)
     ratio = 1
-    iter = 0
-    while((ratio>thresh) & (iter < maxit)):
-        print(iter)
-        iter = iter + 1
-        U_old = U
-        V_old = V
-        Dsq_old = Dsq
-        
+    itr = 0
+    while((thresh < ratio) and (itr < maxit)):
+        print "Starting Iteration " + str(itr)
+        itr = itr + 1
+        U_old[:, :] = U
+        V_old[:, :] = V
+        Dsq_old[:, :] = Dsq
+
         #B step
-        b = B.transpose()
-        for point in range(len(row)):
-            i = row[point]
-            j = col[point]
-            X_hat[i,j] = R[i,j] - np.dot(A[i,:], b[:,j])
-        X_star = X_hat + np.dot(A, B.transpose())
-        B_tilde = np.linalg.solve(Dsq**2 + Lambda*np.eye(k), np.dot(Dsq, np.dot(U.transpose(), X_star)))
-        V, D, _ = np.linalg.svd(np.dot(B_tilde.transpose(), Dsq), False)
-        Dsq = np.diag(np.sqrt(D))
-        B = np.dot(V, Dsq)
-        
+        #b = B.transpose()
+        pattern = (R != 0)
+        ABt = A.dot(B.T)
+        proj_ABt = pattern.multiply(ABt)
+        X_star[:,:] = R.multiply(pattern) - proj_ABt + ABt
+
+        left_side = Dsq**2 + (Lambda * np.eye(k))
+        right_side = np.dot(Dsq, np.dot(U.T, X_star))
+        B_tilde = np.linalg.solve(left_side, right_side)
+        V[:, :], D, _ = np.linalg.svd(np.dot(B_tilde.T, Dsq), False)
+        Dsq[:, :] = np.diag(np.sqrt(D))
+        B[:, :] = np.dot(V, Dsq)
+
         #A step
-        a = A.transpose()
-        for point in range(len(row)):
-            i = row[point]
-            j = col[point]
-            Xt_hat[j,i] = Rt[j,i] - np.dot(B[j,:], a[:,i])
-        Xt_star = Xt_hat + np.dot(B, A.transpose())
-        A_tilde = np.linalg.solve(Dsq**2 + Lambda*np.eye(k), np.dot(Dsq, np.dot(V.transpose(), Xt_star)))
-        U, D, _ = np.linalg.svd(np.dot(A_tilde.transpose(), Dsq), False)
-        Dsq = np.diag(np.sqrt(D))
-        A = np.dot(U,Dsq)
+        pattern = (R.transpose() != 0)
+        BAt = B.dot(A.T)
+        proj_BAt = pattern.multiply(BAt)
+        Xt_star[:,:] = Rt.multiply(pattern) - proj_BAt + BAt
+        
+        left_side = Dsq**2 + (Lambda * np.eye(k))
+        right_side = np.dot(Dsq, np.dot(V.T, Xt_star))
+        A_tilde = np.linalg.solve(left_side, right_side)
+        #could be error here with postion of U
+        U[:,:], d, _ = np.linalg.svd(np.dot(A_tilde.T, Dsq), False)
+        Dsq[:, :] = np.diag(np.sqrt(d))
+        A[:, :] = np.dot(U,Dsq)
         ratio = frob(U_old, Dsq_old, V_old, U, Dsq, V)
-        print(ratio)
+        cost = compute_cost(R, A, B, Lambda)
+        print "Cost => " + str(cost)
+        print "Ratio => " + str(ratio)
+
+    #Final Step: Output Solution
     M = X_star.dot(V)
-    U, D_final, v = np.linalg.svd(M, False)
-    V = np.dot(V, v)
-    Dsq = D_final - Lambda
-    
+    U[:,:], d, v = np.linalg.svd(M, False)
+    V[:,:] = np.dot(V, v)
+    Dsq[:,:] = np.diag(d - Lambda)
+    Dsq[:,:] = np.fmax(Dsq, 0)
     return
 
-def compute_rmse_UV(R, U, V):
+def compute_cost(R, A, B, Lambda):
+    pattern = R != 0
+    proj_ABt = pattern.multiply((A).dot(B.T))
+    cost = np.linalg.norm(proj_ABt - R, 'fro') ** 2
+    norm_A = Lambda * (np.linalg.norm(A, 'fro')) ** 2
+    norm_B = Lambda * (np.linalg.norm(B, 'fro')) ** 2
+    return cost + norm_A + norm_B
 
-    RMSE = np.sqrt((np.square(R[R.nonzero()] - np.dot(U,V.transpose())[R.nonzero()])).mean())
-    return RMSE
+def compute_rmse_UV(R, U, V, Dsq):
+    prediction = U.dot(Dsq.dot(V.T))
+    pattern = (R != 0)
+    s = np.linalg.norm(pattern.multiply(prediction) - R, 'fro') ** 2
+    return np.sqrt(s / R.nnz)
 
 
 def frob(U_old, Dsq_old, V_old, U, Dsq, V):
@@ -82,4 +103,4 @@ def frob(U_old, Dsq_old, V_old, U, Dsq, V):
     vtv = np.multiply(Dsq_old, (V_old.transpose().dot(V)))
     uvprod = np.trace(utu.dot(vtv))
     num = denom + np.trace((Dsq ** 2)) - 2*uvprod
-    return num/ denom
+    return num / denom
