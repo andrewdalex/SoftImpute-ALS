8426e48d06e0a99fc8079ed7495d9460498f307e
Andrew Alex <andrewalex123@ucla.edu>
2017-03-24 16:22:11 -0700

    Refactored the softImpute module

--- dataVis.py
+++ dataVis.py
@@ -6,20 +6,26 @@ import matplotlib.pyplot as plt
 class DataVisualization:
     """ PCA-based visualization on MovieLens data sets """
 
-    def __init__(self, txt_data, M, N):
+    def __init__(self, M, N, txt_data = None, reg_data = None):
         """ Build dense matrix out of data from text file """
+        if txt_data is not None:
+            raw_data = np.genfromtxt(txt_data, dtype=np.int32)
+            item_ids = raw_data[:, 0] - 1
+            comp_ids = raw_data[:, 1] - 1
+            values = raw_data[:, 2].astype(np.float64)
 
-        raw_data = np.genfromtxt(txt_data, dtype=np.int32)
-        item_ids = raw_data[:, 0] - 1
-        comp_ids = raw_data[:, 1] - 1
-        values = raw_data[:, 2].astype(np.float64)
+            #convenient to initially build sparse matrix but will need dense later
+            R = sp.coo_matrix((values, (comp_ids, item_ids)), shape=(M, N))
+            self._data = np.zeros((M, N), order='F')
+            self._data[:, :] = R.toarray()
+            self._M = M
+            self._N = N
 
-        #convenient to initially build sparse matrix but will need dense later
-        R = sp.coo_matrix((values, (comp_ids, item_ids)), shape=(M, N))
-        self._data = np.zeros((M, N), order='F')
-        self._data[:, :] = R.toarray()
-        self._M = M
-        self._N = N
+        elif reg_data is not None:
+            self._data = np.zeros((M, N), order='F')
+            self._data[:, :] = reg_data
+            self._M = M
+            self._N = N
 
 
     def gen_plot(self, figname, special=[]):
@@ -37,7 +43,7 @@ class DataVisualization:
         for i in xrange(self._N):
             if i in special:
                 continue
-            plt.plot(princ_comps[0,i], princ_comps[1,i], 'sb')
+            plt.plot(princ_comps[0,i], princ_comps[1,i], 'sy')
 
         # Give different marker to those in ignore list
         for i in special:
@@ -51,7 +57,7 @@ class DataVisualization:
 # use module to plot MovieLens 100k data set only if directly called
 # otherwise just provide class interface
 def main():
-    dv = DataVisualization('data/ml-100k/u.data', 1682, 943)
+    dv = DataVisualization(1682, 943, txt_data='data/ml-100k/u.data')
     dv.gen_plot('plots/init100k.pdf', special=[5,9])
 
 
---
+++ softImpute.py
@@ -0,0 +1,158 @@
+import numpy as np
+import scipy.sparse as sp
+import scipy.sparse.linalg as splinalg
+import numpy.linalg as npla
+import matplotlib.pyplot as plt
+
+
+class SoftImpute_ALS:
+
+    def __init__(self, k, R):
+        self._R = R
+        self._k = k
+        self._m, self._n = self._R.shape
+        self._bootstrap(k)
+        self._Rt = self._R.T
+        self._X_star = np.zeros((self._m, self._n))
+        self._Xt_star = np.zeros((self._n, self._m))
+
+    def _text_to_CSR(filename, num_users, num_items):
+    	raw_data = np.genfromtxt(filename, dtype=np.int32)
+    	users = raw_data[:, 0] - 1
+    	items = raw_data[:, 1] - 1
+    	ratings = raw_data[:, 2].astype(np.float64)
+    	R = sp.coo_matrix((ratings, (users, items)), shape=(num_users, num_items))
+    	return R.tocsr()
+
+
+    def _bootstrap(self, k):
+        self._U = np.zeros((self._m,k))
+        self._V = np.zeros((self._n,k))
+        self._Dsq = np.eye(k)
+        self._U[:, :] = np.random.randn(self._m, k)
+        self._U_old = np.zeros((self._m, k))
+        self._V_old = np.zeros((self._n, k))
+        self._Dsq_old = np.eye(k)
+
+    def _frob(self):
+        denom = np.trace((self._Dsq_old**2))
+        utu = np.multiply(self._Dsq, (self._U.T.dot(self._U_old)))
+        vtv = np.multiply(self._Dsq_old, (self._V_old.T.dot(self._V)))
+        uvprod = np.trace(utu.dot(vtv))
+        num = denom + np.trace((self._Dsq ** 2)) - 2*uvprod
+        return num / denom
+
+    def _compute_cost(self):
+        pattern = self._R != 0
+        proj_ABt = pattern.multiply((self._A).dot(self._B.T))
+        cost = np.linalg.norm(proj_ABt - self._R, 'fro') ** 2
+        norm_A = self._Lambda * (np.linalg.norm(self._A, 'fro')) ** 2
+        norm_B = self._Lambda * (np.linalg.norm(self._B, 'fro')) ** 2
+        return cost + norm_A + norm_B
+
+    def fit(self, k=40, thresh=1e-05, Lambda=20, maxit=50, plot_conv=None):
+
+        if (k != self._k):
+            self._bootstrap(k)
+            self._k = k
+        #preallocate local data structures
+        #define in class scope for easy parameter passing between aux functions
+        #references will be desroyed before fit terminates
+        self._A = np.dot(self._U, self._Dsq)
+        self._B = np.dot(self._V, self._Dsq)
+
+        ratio = 1
+        itr = 0
+        self._Lambda = Lambda
+
+        if plot_conv is not None:
+            x_plot = []
+            y_plot = []
+
+        while((thresh < ratio) and (itr < maxit)):
+            print "=== Starting Iteration " + str(itr) + " ==="
+            itr = itr + 1
+            self._U_old[:, :] = self._U
+            self._V_old[:, :] = self._V
+            self._Dsq_old[:, :] = self._Dsq
+
+            #B step
+            pattern = (self._R != 0)
+            ABt = self._A.dot(self._B.T)
+            proj_ABt = pattern.multiply(ABt)
+            self._X_star[:,:] = self._R.multiply(pattern) - proj_ABt + ABt
+            left_side = self._Dsq**2 + (self._Lambda * np.eye(k))
+            right_side = np.dot(self._Dsq, np.dot(self._U.T, self._X_star))
+            B_tilde = npla.solve(left_side, right_side)
+            self._V[:, :], D, _ = npla.svd(np.dot(B_tilde.T, self._Dsq), False)
+            self._Dsq[:, :] = np.diag(np.sqrt(D))
+            self._B[:, :] = np.dot(self._V, self._Dsq)
+
+            #A step
+            pattern = (self._R.T != 0)
+            BAt = self._B.dot(self._A.T)
+            proj_BAt = pattern.multiply(BAt)
+            self._Xt_star[:,:] = self._Rt.multiply(pattern) - proj_BAt + BAt
+            left_side = self._Dsq**2 + (self._Lambda * np.eye(k))
+            right_side = np.dot(self._Dsq, np.dot(self._V.T, self._Xt_star))
+            A_tilde = npla.solve(left_side, right_side)
+
+
+            self._U[:,:], d, _ = npla.svd(np.dot(A_tilde.T, self._Dsq), False)
+            self._Dsq[:, :] = np.diag(np.sqrt(d))
+            self._A[:, :] = np.dot(self._U, self._Dsq)
+            ratio = self._frob()
+            cost = self._compute_cost()
+            print "Cost => " + str(cost)
+            print "Ratio => " + str(ratio)
+
+            if plot_conv is not None:
+                y_plot.append(cost)
+                x_plot.append(itr)
+
+        #Final Step: Output Solution
+        M = self._X_star.dot(self._V)
+        self._U[:,:], d, v = npla.svd(M, False)
+        self._V[:,:] = np.dot(self._V, v)
+        self._Dsq[:,:] = np.diag(d - self._Lambda)
+        self._Dsq[:,:] = np.fmax(self._Dsq, 0)
+
+        if plot_conv is not None:
+            plt.figure()
+            plt.plot(x_plot, y_plot, 'sg')
+            plt.xlabel('Number of Iterations')
+            plt.ylabel('Computed Cost')
+            plt.savefig(plot_conv)
+        return
+
+
+    def compute_rmse(self, R_test):
+        prediction = self._U.dot(self._Dsq.dot(self._V.T))
+        pattern = (R_test != 0)
+        s = npla.norm(pattern.multiply(prediction) - R_test, 'fro') ** 2
+        return np.sqrt(s / R_test.nnz)
+
+    def get_UVD(self):
+        #return copies so as to not corrupt internal structures
+        return (self._U.copy(), self._V.copy(), self._Dsq.copy())
+
+
+def text_to_CSR(filename, m, n):
+	raw_data = np.genfromtxt(filename, dtype=np.int32)
+	users = raw_data[:, 0] - 1
+	items = raw_data[:, 1] - 1
+	ratings = raw_data[:, 2].astype(np.float64)
+	R = sp.coo_matrix((ratings, (users, items)), shape=(m, n))
+	return R.tocsr()
+
+def main():
+    num_users = 943
+    num_items = 1682
+    R_train = text_to_CSR('data/ml-100k/ub.base', num_users, num_items)
+    R_test = text_to_CSR('data/ml-100k/ub.test', num_users, num_items)
+    sals = SoftImpute_ALS(40, R_train)
+    sals.fit(plot_conv="plots/test.jpg")
+    print sals.compute_rmse(R_test)
+
+if __name__ == "__main__":
+    main()
